51460936ddbac7e80b5d5dc88bee1bc9b5677476
Mohammad Ahmad <eng.ma90@gmail.com>
2020-04-16 16:17:33 +0200

    Fixed express.static error

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 //Handle production
 if (process.env.NODE_ENV === 'production') {
     //Static folder
-    app.use(express.static(__dirname, '/public'));
+    app.use(express.static(__dirname + '/public'));
 
     //Handle SPA
     app.get('/.*/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
