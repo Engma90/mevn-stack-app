f3afaf5056329864866a10d54705aa1a5a9c5e54
Mohammad Ahmad <eng.ma90@gmail.com>
2020-04-16 19:46:20 +0200

    Fixed SPA expression

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //Handle production
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
     //Static folder
     app.use(express.static(__dirname + '/public'));
 
