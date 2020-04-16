cad8a5f54c011f74686ceee939490c1860fc8e01
Mohammad Ahmad <eng.ma90@gmail.com>
2020-04-16 20:04:40 +0200

    Fixing production environment

--- server/index.js
+++ server/index.js
@@ -26,6 +26,6 @@ if (process.env.NODE_ENV === 'production') {
     //app.all()
 }
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
